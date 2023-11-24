e757a450d0b1a4a885eb46cfa992a74714807ba8
Kaya Jhuti <itskayaj@gmail.com>
2023-11-23 21:53:46 -0500

    Add glow to mouse trail

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const colors = ['#74a7fe', '#53d5fd', '#b18cfe'];
+const animationClasses = ['dot-1', 'dot-2', 'dot-3'];
 let prevX = 0;
 let prevY = 0;
 
@@ -7,7 +9,7 @@ function calculateDistance(x1, y1, x2, y2) {
   return Math.sqrt(dx * dx + dy * dy);
 }
 
-function createDot(e, colors, animationClasses) {
+function createDot(e, colors) {
   const dot = document.createElement('i');
   const animationClass = getRandomItem(animationClasses);
   dot.className = `dot fa-solid fa-star fall ${animationClass}`;
@@ -19,21 +21,30 @@ function createDot(e, colors, animationClasses) {
   return dot;
 }
 
+function createMouseTrail(e) {
+  const trail = document.createElement('div');
+  trail.className = 'trail';
+  trail.style.left = `${e.pageX}`;
+  trail.style.top = `${e.pageY}`;
+  return trail;
+}
+
 function getRandomItem(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-function removeDot(dot) {
-  setTimeout(() => document.body.removeChild(dot), 1500);
+function removeElement(dot, timeout) {
+  setTimeout(() => document.body.removeChild(dot), timeout);
 }
-
-const colors = ['#74a7fe', '#53d5fd', '#b18cfe'];
-const animationClasses = ['dot-1', 'dot-2', 'dot-3'];
 
 window.onmousemove = e => {
   const distance = calculateDistance(prevX, prevY, e.pageX, e.pageY);
+  const trail = createMouseTrail(e);
+  document.body.appendChild(trail);
+  removeElement(trail, 200);
+  
   if (distance < 50) return;
-  const dot = createDot(e, colors, animationClasses);
+  const dot = createDot(e, colors);
   document.body.appendChild(dot);
-  removeDot(dot);
+  removeElement(dot, 1500);
 }
